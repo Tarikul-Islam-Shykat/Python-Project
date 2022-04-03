e42d0faf01a1b3ce138f51e2c2ae7a1cf8fd0945
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2022-04-03 19:56:31 +0600

    Update main.py

--- Open CV/Projects/Face Detection/main.py
+++ Open CV/Projects/Face Detection/main.py
@@ -8,10 +8,10 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 haar_cascade = cv.CascadeClassifier('res/haar_face.xml')
 
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=2.3, minNeighbors=3)
-
 '''
 this will take an image and use this variable for scale factor and minimum label to essentialyy
-detect a face and return the rectangular co-ordinates of that face as a list 
+detect a face and return the rectangular co-ordinates of that face as a list .
+This function will return a rectangle with coordinates(x,y,w,h) around the detected face. 
 '''
 
 print(f'Number of faces found = {len(faces_rect)}')
